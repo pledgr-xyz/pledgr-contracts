6f2c5cbc74bca09a782894929b903ded53e01cc2
aeolian <aeolian.eth@protonmail.com>
2021-12-28 21:26:08 +1000

    Add basic test for receiver function

--- test/pledge.js
+++ test/pledge.js
@@ -1,21 +1,48 @@
 const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { ethers, waffle } = require("hardhat");
 
-describe("Pledge", function () {
+const { provider } = waffle;
+
+describe("Pledge", () => {
   describe("deployment", () => {
     it("Should set storage correctly", async function () {
+      const [_, addrA] = await ethers.getSigners();
+
       const Pledge = await ethers.getContractFactory("PledgeV1");
-      const pledge = await Pledge.deploy(
-        ["0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"],
-        [40]
-      );
+      const pledge = await Pledge.deploy([addrA.address], [40]);
       await pledge.deployed();
 
-      expect(
-        await pledge.getReceiverPercent(
-          "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
-        )
-      ).to.equal(40);
+      expect(await pledge.getReceiverPercent(addrA.address)).to.equal(40);
+    });
+  });
+
+  describe("recieve", () => {
+    it("with one receiver", async () => {
+      const [_, receiver, sender] = await ethers.getSigners();
+      expect(await provider.getBalance(receiver.address)).to.equal(
+        ethers.utils.parseUnits("10000.0")
+      );
+
+      const Pledge = await ethers.getContractFactory("PledgeV1");
+      const pledge = await Pledge.deploy([receiver.address], [40]);
+      await pledge.deployed();
+
+      const tx = await sender.sendTransaction({
+        from: sender.address,
+        to: pledge.address,
+        value: ethers.utils.parseEther("1"),
+      });
+      const txReceipt = await tx.wait();
+
+      expect(await provider.getBalance(receiver.address)).to.equal(
+        ethers.utils.parseUnits("10000.4")
+      );
+      expect(await provider.getBalance(sender.address)).to.equal(
+        ethers.utils
+          .parseEther("10000")
+          .sub(ethers.utils.parseEther("0.6"))
+          .sub(txReceipt.gasUsed.mul(txReceipt.effectiveGasPrice))
+      );
     });
   });
 });
